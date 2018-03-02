42c7cccc1fdb060f56ad2010fefb2b5dcbf76e3c
Martux99 <34321961+Martux99@users.noreply.github.com>
2018-03-02 15:47:20 -0600

    La salsa
    verde

---
+++ Project Me And Your Mama/Assets/Player_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Movement : MonoBehaviour
+{
+    public Transform Position;
+    public Rigidbody2D rb2d;
+    private int screen;
+    public float speed = 50f;
+    float spd = 0;
+    int buleano = 0;
+    private Vector2 distance;
+    private GameObject movetoesto;
+    public Transform Target;
+    public Vector3 Camara;
+    void Start()
+    {
+        movetoesto = GameObject.FindGameObjectWithTag("Esto");
+    }
+    void FixedUpdate()
+    {
+        //Camara = Camera.main.ScreenToViewportPoint(Input.GetTouch(0).position);
+        Touch touch = Input.GetTouch(0);
+        distance = touch.position;
+        if (movetoesto != null)
+        {
+            spd = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, distance, spd);
+            transform.position = new Vector2(Mathf.Clamp(transform.position.x, 5f, 5f), Mathf.Clamp(transform.position.y, 5f, 5f));
+        }
+
+
+    }
+}
